dba45529ad0a9bdabc18c04a4b50a03655e7e665
chendengfeng <18790120405@163.com>
2021-03-14 23:14:24 +0800

    修改验证码生成

--- login_register/src/main/java/com/deepglint/register_login/util/CodeUtil.java
+++ login_register/src/main/java/com/deepglint/register_login/util/CodeUtil.java
@@ -10,7 +10,6 @@ import java.util.Random;
  * @ClassName CodeUtil
  * @Description 生成四位随机验证码
  * @author: cdf
- * @Date: 2020-06-16 23:33
  **/
 @Component
 @Slf4j
@@ -20,8 +19,9 @@ public class CodeUtil {
 		Random random = new Random();
 		StringBuilder stringBuilder = new StringBuilder();
 		for (int i = 0; i < 4; i++) {
-			stringBuilder = stringBuilder.append(random.nextInt(9));
+			stringBuilder = stringBuilder.append(random.nextInt(10));
 		}
+		log.info("验证码已生成: {} ", stringBuilder);
 		return JSON.toJSONString(stringBuilder);
 	}
 }
